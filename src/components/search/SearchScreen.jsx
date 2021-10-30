0ba46eab645a99e016af0fd25ee91b37186eb5a8
Stivencio <estebangonzalez-plz@hotmail.com>
2021-10-30 06:20:30 -0300

    queryString agregado

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
 import { heroes } from "../../data/heroes";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../heroes/HeroCard";
 
-export const SearchScreen = () => {
-	const heroesFiltered = heroes;
+export const SearchScreen = ({ history }) => {
+	//location es un hook de React Router, sirve para obtener algunos parametros como el pathname, search, etc.
+	const location = useLocation();
+	//queryString parsea todos los parametros de la url y los separa en un objeto para manipularlos de mejor forma, en este caso solo vamos a utilizar el primer parametro que definimos como "q" de "query" y le damos el valor '' en caso de que no reciba ningun parametro, para que no quede en null
+	const { q = "" } = queryString.parse(location.search);
 
 	const [formValues, handleInputChange, reset] = useForm({
-		searchText: "",
+		searchText: q,
 	});
-
 	const { searchText } = formValues;
+	const heroesFiltered = heroes;
 
 	const handleSearch = (e) => {
 		e.preventDefault();
+		history.push(`?q=${searchText}`);
 		console.log(searchText);
 	};
 
